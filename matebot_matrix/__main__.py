9b6811255dab15ee4927b6a6d888f1c1f1faf80d
Chris <chris@hopfen.space>
2022-01-23 22:29:28 +0100

    Moved some setup code and SDK creation into a new MatrixBot subclass

--- matebot_matrix/__main__.py
+++ matebot_matrix/__main__.py
@@ -1,31 +1,12 @@
 import asyncio
-import logging
 
-from matebot_sdk.sdk import AsyncSDK
-from hopfenmatrix.matrix import MatrixBot
-
-from .config import MateBotConfig
-
-
-async def _run(bot: MatrixBot, sdk: AsyncSDK):
-    await sdk.setup()
-    await bot.start_bot()
+from .bot import MateBot
 
 
 def main():
-    bot = MatrixBot(config_class=MateBotConfig)
+    bot = MateBot()
     bot.set_auto_join()
-    sdk_config = bot.config.api
-    sdk = AsyncSDK(
-        base_url=sdk_config.base_url,
-        app_name=sdk_config.app_name,
-        password=sdk_config.app_password,
-        ca_path=sdk_config.ca_path,
-        configuration=bot.config.client,
-        callback=(sdk_config.callback.public_url, sdk_config.callback.username, sdk_config.callback.password),
-        logger=logging.getLogger("sdk.client")
-    )
-    asyncio.run(_run(bot, sdk))
+    asyncio.run(bot.start_bot())
 
 
 if __name__ == "__main__":
---
+++ matebot_matrix/bot.py
@@ -0,0 +1,45 @@
+import logging
+from typing import Optional, Type
+
+from matebot_sdk.sdk import AsyncSDK
+from hopfenmatrix.config import Config
+from hopfenmatrix.matrix import MatrixBot
+
+from .config import MateBotConfig
+
+
+class MateBot(MatrixBot):
+    def __init__(
+            self,
+            *,
+            display_name: str = None,
+            enable_initial_info: bool = False,
+            config: Optional[Config] = None,
+            config_path: str = "config.json",
+            config_class: Type[Config] = MateBotConfig
+    ):
+        super().__init__(
+            display_name=display_name,
+            enable_initial_info=enable_initial_info,
+            config=config,
+            config_path=config_path,
+            config_class=config_class
+        )
+
+        self.sdk = AsyncSDK(
+            base_url=self.config.api.base_url,
+            app_name=self.config.api.app_name,
+            password=self.config.api.app_password,
+            ca_path=self.config.api.ca_path,
+            configuration=self.config.client,
+            callback=(
+                self.config.api.callback.public_url,
+                self.config.api.callback.username,
+                self.config.api.callback.password
+            ),
+            logger=logging.getLogger("sdk")
+        )
+
+    async def start_bot(self):
+        await self.sdk.setup()
+        await super().start_bot()
