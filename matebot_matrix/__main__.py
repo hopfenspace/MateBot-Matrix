0b137524ef13a08da205d4134fd5528b879df65a
Chris <chris@hopfen.space>
2022-01-27 23:53:28 +0100

    Fixed the main entrypoint

--- matebot_matrix/__main__.py
+++ matebot_matrix/__main__.py
@@ -1,17 +1,25 @@
 import asyncio
+import logging
 
 from .bot import MateBot
 from .commands import COMMANDS
 
 
 def main():
+    logger = logging.getLogger(__name__)
     bot = MateBot()
+    logger.debug("Starting bot...")
     bot.set_auto_join()
 
+    print("commands =", COMMANDS)
     for cmd in COMMANDS:
-        bot.register_command(cmd, cmd.name, description=cmd.description, command_syntax=cmd.usage)
+        bot.register_command(COMMANDS[cmd], cmd, description=COMMANDS[cmd].description, command_syntax=COMMANDS[cmd].usage)
 
-    asyncio.run(bot.start_bot())
+    try:
+        asyncio.run(bot.start_bot())
+    except KeyboardInterrupt:
+        pass
+    logger.debug("Exiting gracefully.")
 
 
 if __name__ == "__main__":
