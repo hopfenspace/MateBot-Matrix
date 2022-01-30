645ad4746c72525118de0fc8d4efc856ac7e438f
Chris <chris@hopfen.space>
2022-01-30 16:10:50 +0100

    Implemented the send command

--- matebot_matrix/commands/send.py
+++ matebot_matrix/commands/send.py
@@ -1,25 +1,19 @@
 """
-MateBot command executor classes for /send
+MateBot command executor for send
 """
 
-import logging
+from hopfenmatrix.matrix import MatrixRoom, RoomMessageText
+from matebot_sdk.exceptions import UserAPIException
 
-import telegram
-
-from mate_bot.parsing.types import amount as amount_type
-from mate_bot.parsing.types import user as user_type
-from mate_bot.parsing.util import Namespace
-from mate_bot.commands.base import BaseCallbackQuery, BaseCommand
-from mate_bot.state.user import MateBotUser
-from mate_bot.state.transactions import LoggedTransaction
-
-
-logger = logging.getLogger("commands")
+from .base import BaseCommand
+from ..bot import MateBot
+from ..parsing.util import Namespace
+from ..parsing.types import amount as amount_type, user as user_type
 
 
 class SendCommand(BaseCommand):
     """
-    Command executor for /send
+    Command executor for send
     """
 
     def __init__(self):
@@ -47,117 +41,30 @@ class SendCommand(BaseCommand):
         self.parser.add_argument("receiver", type=user_type)
         self.parser.add_argument("reason", default="<no description>", nargs="*")
 
-    def run(self, args: Namespace, update: telegram.Update) -> None:
-        """
-        :param args: parsed namespace containing the arguments
-        :type args: argparse.Namespace
-        :param update: incoming Telegram update
-        :type update: telegram.Update
-        :return: None
-        """
-        sender = self.get_sender(api, room, event)
+    async def run(self, args: Namespace, bot: MateBot, room: MatrixRoom, event: RoomMessageText) -> None:
+        sender = await bot.sdk.get_user_by_app_alias(event.sender)
+
+        self.logger.debug(args.receiver)
+        if not args.receiver:
+            self.logger.warning("No receiver!")
+            return
 
         if isinstance(args.reason, list):
-            reason = "send: " + " ".join(args.reason)
+            reason = "send: " + " ".join(map(str, args.reason))
         else:
-            reason = "send: " + args.reason
-
-        if sender == args.receiver:
-            update.effective_message.reply_text("You can't send money to yourself!")
-            return
-
-        if not self.ensure_permissions(sender, 1, update.effective_message):
-            return
-
-        def e(variant: str) -> str:
-            return f"send {variant} {args.amount} {sender.uid} {args.receiver.uid}"
-
-        update.effective_message.reply_text(
-            f"Do you want to send {args.amount / 100 :.2f}€ to {str(args.receiver)}?"
-            f"\nDescription: `{reason}`",
-            reply_markup=telegram.InlineKeyboardMarkup([
-                [
-                    telegram.InlineKeyboardButton("CONFIRM", callback_data=e("confirm")),
-                    telegram.InlineKeyboardButton("ABORT", callback_data=e("abort"))
-                ]
-            ]),
-            parse_mode="Markdown"
-        )
-
-
-class SendCallbackQuery(BaseCallbackQuery):
-    """
-    Callback query executor for /send
-    """
-
-    def __init__(self):
-        super().__init__("send", "^send")
-
-    def run(self, update: telegram.Update) -> None:
-        """
-        Process or abort transaction requests based on incoming callback queries
-
-        :param update: incoming Telegram update
-        :type update: telegram.Update
-        :return: None
-        """
+            reason = "send: " + str(args.reason)
 
         try:
-            variant, amount, original_sender, receiver = self.data.split(" ")
-            amount = int(amount)
-            receiver = MateBotUser(int(receiver))
-            original_sender = MateBotUser(int(original_sender))
-
-            if variant == "confirm":
-                confirmation = True
-            elif variant == "abort":
-                confirmation = False
-            else:
-                raise ValueError(f"Invalid confirmation setting: '{variant}'")
-
-            sender = MateBotUser(update.callback_query.from_user)
-            if sender != original_sender:
-                update.callback_query.answer(f"Only the creator of this transaction can {variant} it!")
-                return
-
-            reason = None
-            for entity in update.callback_query.message.parse_entities():
-                if entity.type == "code":
-                    if reason is None:
-                        reason = update.callback_query.message.parse_entity(entity)
-                    else:
-                        raise RuntimeError("Multiple reason definitions")
-
-            if reason is None:
-                raise RuntimeError("Unknown reason while confirming a Transaction")
-
-            if confirmation:
-                LoggedTransaction(
-                    sender,
-                    receiver,
-                    amount,
-                    reason,
-                    update.callback_query.bot
-                ).commit()
-
-                update.callback_query.message.edit_text(
-                    f"Okay, you sent {amount / 100 :.2f}€ to {str(receiver)}",
-                    reply_markup=telegram.InlineKeyboardMarkup([])
-                )
-
-            else:
-                update.callback_query.message.edit_text(
-                    "You aborted the operation. No money has been sent.",
-                    reply_markup=telegram.InlineKeyboardMarkup([])
-                )
-
-        except (IndexError, ValueError, TypeError, RuntimeError):
-            update.callback_query.answer(
-                text="There was an error processing your request!",
-                show_alert=True
+            transaction = await bot.sdk.make_new_transaction(sender, args.receiver, args.amount, reason)
+            await bot.send_reply(
+                f"Okay, you sent {transaction.amount / 100 :.2f}€ to {bot.sdk.get_username(args.receiver)}",
+                room.room_id,
+                event
             )
-            update.callback_query.message.edit_text(
-                "There was an error processing this request. No money has been sent.",
-                reply_markup=telegram.InlineKeyboardMarkup([])
+        except UserAPIException as exc:
+            self.logger.warning(f"{type(exc).__name__}: {exc.message} ({exc.status}, {exc.details})")
+            await bot.send_reply(
+                f"Your request couldn't be processed. No money has been transferred:\n{exc.message}",
+                room.room_id,
+                event
             )
-            raise
