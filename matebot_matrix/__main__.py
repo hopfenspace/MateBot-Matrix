f20a0832c91894cba3b4b7552febf98ee56c022d
Chris <chris@hopfen.space>
2022-01-31 14:49:35 +0100

    Added the API callbacks and running them in a separate thread

--- matebot_matrix/__main__.py
+++ matebot_matrix/__main__.py
@@ -1,10 +1,16 @@
+import logging
+
 from .bot import MateBot
 from .commands import COMMANDS
+from .callback import APICallbackDispatcher, get_callback_app
 
 
 def main():
     bot = MateBot()
     bot.set_auto_join()
+
+    # TODO: integrate specific log level adjustments in the configuration
+    logging.getLogger("aiohttp.access").setLevel(logging.WARNING)
 
     for name in COMMANDS:
         cmd = COMMANDS[name]
@@ -13,6 +19,11 @@ def main():
         else:
             bot.register_command(cmd, name, description=cmd.description, command_syntax=cmd.usage)
 
+    dispatcher = APICallbackDispatcher(bot)
+    app = get_callback_app(dispatcher, logging.getLogger("callback"))
+    if bot.config.api.callback.username or bot.config.api.callback.password:
+        bot.logger.warning("Username & password authentication for callbacks is not supported yet!")
+    dispatcher.start_async_thread(app, bot.config.api.callback.address, bot.config.api.callback.port)
     bot.run()
 
 
--- matebot_matrix/callback.py
+++ matebot_matrix/callback.py
@@ -1,14 +1,17 @@
 import asyncio
 import inspect
 import logging
+import threading
 import collections
 from typing import Awaitable, Callable, Dict, List, Optional, Tuple
 
+from aiohttp import web
 from matebot_sdk.base import CallbackUpdate
 
 from .bot import MateBot
 
 
+ASYNC_SLEEP_DURATION: float = 0.5
 CALLBACK_TYPE = Callable[[CallbackUpdate, str, int, MateBot, logging.Logger, ...], Optional[Awaitable[None]]]
 STORAGE_TYPE = Dict[
     Tuple[CallbackUpdate, Optional[str]],
@@ -21,6 +24,11 @@ class APICallbackDispatcher:
         self.bot = bot
         self.logger = logging.getLogger("api-callback")
         self._storage: STORAGE_TYPE = collections.defaultdict(list)
+
+        self.event_thread: Optional[threading.Thread] = None
+        self.event_loop: Optional[asyncio.AbstractEventLoop] = None
+        self.event_thread_running: threading.Event = threading.Event()
+        self.event_thread_started: threading.Event = threading.Event()
 
     def register(self, event: Tuple[CallbackUpdate, Optional[str]], func: CALLBACK_TYPE, *args, **kwargs):
         self._storage[event].append((func, args, kwargs))
@@ -41,3 +49,51 @@ class APICallbackDispatcher:
                     except Exception as exc:
                         self.logger.warning(f"{type(exc).__name__} in API callback handler for event {event}")
                         raise
+
+    async def run_async(self, app: web.Application, host: str, port: int):
+        print("run async")
+        self.event_loop = asyncio.get_event_loop()
+        self.logger.debug(f"Event loop {self.event_loop} of {threading.current_thread()} has been attached to {self}")
+        self.event_thread_started.set()
+
+        runner = web.AppRunner(app)
+        await runner.setup()
+        site = web.TCPSite(runner, host, port)
+        await site.start()
+
+        self.logger.debug(f"Sleeping until {self.event_thread_running} gets set...")
+        while not self.event_thread_running.is_set():
+            await asyncio.sleep(ASYNC_SLEEP_DURATION)
+        self.logger.debug("Stopping callback server...")
+        await site.stop()
+        self.logger.info(f"Closing async thread {threading.current_thread()}...")
+
+    def start_async_thread(self, app: web.Application, host: str, port: int, daemon: bool = True, name: str = None):
+        if self.event_thread is not None:
+            raise RuntimeError("Server thread already running")
+        print("start thread")
+        self.event_thread = threading.Thread(
+            target=lambda: asyncio.run(self.run_async(app, host, port)),
+            name=name,
+            daemon=daemon
+        )
+        self.event_thread.start()
+
+
+def get_callback_app(dispatcher: APICallbackDispatcher, logger: logging.Logger):
+    async def process(request: web.Request) -> web.Response:
+        methods = {
+            "create": CallbackUpdate.CREATE,
+            "update": CallbackUpdate.UPDATE,
+            "delete": CallbackUpdate.DELETE
+        }
+        action = str(request.match_info["operation"]).lower()
+        model = str(request.match_info["model"]).lower()
+        model_id = int(request.match_info["model_id"])
+        logger.debug(f"Incoming callback query: '{action.upper()} {model} {model_id}'")
+        dispatcher.dispatch(methods[action], model, model_id)
+        return web.Response()
+
+    app = web.Application()
+    app.add_routes([web.get(r"/{operation}/{model}/{model_id:\d+}", process)])
+    return app
